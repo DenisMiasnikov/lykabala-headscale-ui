f08017a878e9d1e43b306af95364a27bf963cd46
denismyasnikov <deniemyasnikov@gmail.com>
2026-04-08 16:07:49 +0300

    refactor namespace page

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -3,7 +3,7 @@ import styles from './button.module.css'
 
 interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   label: string,
-  mode: "primary" | "secondary" | "danger" | "default" |'action'
+  mode?: "primary" | "secondary" | "danger" | "default" |'action'
 }
 
 export const Button = ({onClick, type,  label,  mode}:IButtonProps) => {
