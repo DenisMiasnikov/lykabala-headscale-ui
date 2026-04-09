ea7ea6d9eac3f4c12942288478e0a569393f3690
denismyasnikov <deniemyasnikov@gmail.com>
2026-04-09 22:24:35 +0300

    fix image uploader

---
+++ src/pages/api/test-minio.ts
@@ -0,0 +1,13 @@
+// /pages/api/test-minio.ts
+import type { NextApiRequest, NextApiResponse } from "next";
+import { s3Client } from "../../shared/lib/minio";
+import { ListBucketsCommand } from "@aws-sdk/client-s3";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const data = await s3Client.send(new ListBucketsCommand({}));
+    res.json({ buckets: data.Buckets });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+}
--- src/shared/lib/minio.ts
+++ src/shared/lib/minio.ts
@@ -1,7 +1,7 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 // endpoint: process.env.MINIO_ENDPOINT || "http://minio:9000",
-const s3Client = new S3Client({
+export const s3Client = new S3Client({
   endpoint: "http://minio:9000",
   region: "us-east-1",
   credentials: {
