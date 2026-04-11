9accf391416c8edb29aa2362bffeb5988ec1627e
denismyasnikov <deniemyasnikov@gmail.com>
2026-04-11 22:58:24 +0300

    update headscale auth

--- src/pages/api/internal/env-config.ts
+++ src/pages/api/internal/env-config.ts
@@ -2,16 +2,11 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import fs from "fs";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const hasEnvConfig = !!(process.env.HEADSCALE_URL && process.env.HEADSCALE_API_KEY);
-  const HEADSCALE_API_KEY_FILE = process.env.HEADSCALE_API_KEY_FILE || "";
-  const HEADSCALE_API_KEY = process.env.HEADSCALE_API_KEY || "";
-  const fromFs =  fs.readFileSync(HEADSCALE_API_KEY_FILE, "utf8").trim();
+  const HEADSCALE_API_KEY_FROM_FILE =  fs.readFileSync(process.env.HEADSCALE_API_KEY_FILE || "", "utf8").trim();
+  const hasEnvConfig = !!(process.env.HEADSCALE_URL && process.env.HEADSCALE_API_KEY) || HEADSCALE_API_KEY_FROM_FILE;
 
   res.json({
     available: hasEnvConfig,
-    url: hasEnvConfig ? process.env.HEADSCALE_URL : null,
-    apiKey: hasEnvConfig ? HEADSCALE_API_KEY : null,
-    apiKeyFile: hasEnvConfig ? HEADSCALE_API_KEY_FILE : null,
-    fromFs
+    url: hasEnvConfig ? (process.env.HEADSCALE_URL || "http://headscale:8080") : null,
   });
 }
