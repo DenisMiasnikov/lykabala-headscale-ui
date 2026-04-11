a3dcfc0de1e5bfb6cb62e0ab5bc36f384dac72cd
denismyasnikov <deniemyasnikov@gmail.com>
2026-04-11 22:51:23 +0300

    update headscale auth

--- src/pages/api/internal/env-config.ts
+++ src/pages/api/internal/env-config.ts
@@ -1,9 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const hasEnvConfig = !!(process.env.HEADSCALE_URL && process.env.HEADSCALE_API_KEY);
-  res.json({ 
+  const HEADSCALE_API_KEY_FILE = process.env.HEADSCALE_API_KEY_FILE || "";
+  const HEADSCALE_API_KEY = process.env.HEADSCALE_API_KEY || "";
+  const fromFs =  fs.readFileSync(HEADSCALE_API_KEY_FILE, "utf8").trim();
+
+  res.json({
     available: hasEnvConfig,
-    url: hasEnvConfig ? process.env.HEADSCALE_URL : null
+    url: hasEnvConfig ? process.env.HEADSCALE_URL : null,
+    apiKey: hasEnvConfig ? HEADSCALE_API_KEY : null,
+    apiKeyFile: hasEnvConfig ? HEADSCALE_API_KEY_FILE : null,
+    fromFs
   });
 }
